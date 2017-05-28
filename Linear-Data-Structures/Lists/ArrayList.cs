77cf37cab8999b2eed4ace435e6fa708dda30850
Jordan Jordanov <d_yugioh@abv.bg>
2017-05-28 17:51:56 +0300

    ArrayList_Implementation

--- Linear-Data-Structures/Lists/ArrayList.cs
+++ Linear-Data-Structures/Lists/ArrayList.cs
@@ -2,28 +2,95 @@
 
 public class ArrayList<T>
 {
+    private const int Initial_Capacity = 2;
+
+    private T[] items;
+
+    public ArrayList()
+    {
+        this.items = new T[Initial_Capacity];
+    }
+
     public int Count { get; set; }
 
     public T this[int index]
     {
         get
         {
-            throw new NotImplementedException();
+            if (index >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return this.items[index];
         }
 
         set
         {
-            throw new NotImplementedException();
+            if (index >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            this.items[index] = value;
         }
     }
 
     public void Add(T item)
     {
-        throw new NotImplementedException();
+        if (this.Count == this.items.Length)
+        {
+            this.Resize();
+        }
+
+        this.items[this.Count++] = item;
+    }
+
+    private void Resize()
+    {
+        T[] copy = new T[this.items.Length * 2];
+        for (int i = 0; i < this.items.Length; i++)
+        {
+            copy[i] = this.items[i];
+        }
+        this.items = copy;
     }
 
     public T RemoveAt(int index)
     {
-        throw new NotImplementedException();
+        if (index >= this.Count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        T element = this.items[index];
+        this.items[index] = default(T);
+        this.Shift(index);
+        this.Count--;
+
+        if (this.Count <= this.items.Length / 4)
+        {
+            this.Shrink();
+        }
+
+        return element;
+    }
+
+    private void Shrink()
+    {
+        T[] copy = new T[this.items.Length / 2];
+        for (int i = 0; i < this.Count; i++)
+        {
+            copy[i] = this.items[i];
+        }
+
+        this.items = copy;
+    }
+
+    private void Shift(int index)
+    {
+        for (int i = index; i < this.Count; i++)
+        {
+            this.items[i] = this.items[i + 1];
+        }
     }
 }
--- Linear-Data-Structures/Lists/Program.cs
+++ Linear-Data-Structures/Lists/Program.cs
@@ -2,6 +2,9 @@
 {
     public static void Main(string[] args)
     {
-
+        ArrayList<int> list = new ArrayList<int>();
+        list.Add(5);
+        list[0] = list[0] + 1;
+        int element = list.RemoveAt(0);
     }
 }
