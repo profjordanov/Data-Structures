125aeb2a0cf56913775967ee0e5b71b7f11c9111
Jordan Jordanov <d_yugioh@abv.bg>
2017-06-04 18:01:18 +0300

    LinkedList and Queue

--- Linked-List-CSharp-Skeleton/LinkedList/LinkedList.cs
+++ Linked-List-CSharp-Skeleton/LinkedList/LinkedList.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 
+
+//This is STACK
 public class LinkedList<T> : IEnumerable<T>
 {
     public class Node
@@ -50,25 +52,84 @@ public class LinkedList<T> : IEnumerable<T>
 
     public T RemoveFirst()
     {
-        // TODO: Throw exception if the list is empty
-        throw new NotImplementedException();
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException();
+        }
+
+        T item = this.Head.Value;
+
+        this.Head = this.Head.Next;
+
+        this.Count--;
+
+        if (IsEmpty())
+        {
+            this.Tail = null;
+        }
+
+        return item;
     }
 
     public T RemoveLast()
     {
-        // TODO: Throw exception if the list is empty
-        throw new NotImplementedException();
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException();
+        }
+
+        T item = this.Tail.Value;
+
+        if (this.Count == 1)
+        {
+            this.Head = this.Tail = null;
+        }
+        else
+        {
+            Node newTail = this.GetSecondToLast();
+            newTail.Next = null;
+            this.Tail = newTail;
+        }
+
+        this.Count--;
+
+
+        return item;
+    }
+
+    private Node GetSecondToLast()
+    {
+         Node current = this.Head;
+         while (current.Next != this.Tail)
+         {
+             current = current.Next;
+         }
+
+        return current;
+    }
+
+    private bool IsEmpty()
+    {
+        if (this.Count == 0)
+        {
+            return true;
+        }
+
+        return false;
     }
 
     public IEnumerator<T> GetEnumerator()
     {
-        // TODO
-        throw new NotImplementedException();
+        Node current = this.Head;
+        while (current != null)
+        {
+            yield return current.Value;
+            current = current.Next;
+        }
     }
 
     IEnumerator IEnumerable.GetEnumerator()
     {
-        // TODO
-        throw new NotImplementedException();
+        return this.GetEnumerator();
     }
 }
