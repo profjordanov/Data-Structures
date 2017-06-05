592a392b524c8a1eb3d803684332740178b8d446
Jordan Jordanov <d_yugioh@abv.bg>
2017-06-05 11:34:14 +0300

    DoubleLinedList

---
+++ Linear_Data_Structures_Exercises/Sort Words/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+            List<string> list = input.Select(word => word).ToList();
+            list.Sort();
+            foreach (var word in list)
+            {
+                Console.Write(word + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Linear_Data_Structures_Exercises/Sum and Average/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_and_Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+            List<double> list = input.Select(number => double.Parse(number)).ToList();
+            double sum = list.Sum();
+            double avrg = list.Sum() / list.Count();
+            Console.WriteLine($"Sum={sum}; Average={avrg}");
+        }
+    }
+}
