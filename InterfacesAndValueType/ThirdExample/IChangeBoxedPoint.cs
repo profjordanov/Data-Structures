b546c901ffba11f2dc50f80babe1e7dcbc30fcb2
Jordan Jordanov <d_yugioh@abv.bg>
2018-08-07 09:29:42 +0300

    Add files via upload

---
+++ InterfacesAndValueType/ThirdExample/IChangeBoxedPoint.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ThirdExample
+{
+    // Interface defining a Change method 
+    public interface IChangeBoxedPoint
+    {
+        void Change(Int32 x , Int32 y);
+    }
+}
---
+++ InterfacesAndValueType/ThirdExample/Point.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ThirdExample
+{
+    // Point is a value type. 
+    public struct Point : IChangeBoxedPoint
+    {
+        private Int32 m_x, m_y;
+        public Point(Int32 x , Int32 y)
+        {
+            m_x = x;
+            m_y = y;
+        }
+        public void Change(Int32 x , Int32 y)
+        {
+            m_x = x; m_y = y;
+        }
+        public override String ToString()
+        {
+            return String.Format("({0}, {1})" , m_x.ToString() , m_y.ToString());
+        }
+    }
+}
