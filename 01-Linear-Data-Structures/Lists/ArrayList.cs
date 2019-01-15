0423b1154f7c8f7199dda7259cd9700da616e1f5
Jordan Jordanov <d_yugioh@abv.bg>
2019-01-15 15:17:31 +0200

    init

---
+++ 01-Linear-Data-Structures/Lists/ArrayList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class ArrayList<T>
+{
+    public int Count { get; set; }
+
+    public T this[int index]
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+
+        set
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public void Add(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T RemoveAt(int index)
+    {
+        throw new NotImplementedException();
+    }
+}
