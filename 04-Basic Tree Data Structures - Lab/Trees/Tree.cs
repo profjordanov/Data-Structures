853dd13e709d95c364ece66dd1e8f2e83f4f42f5
Jordan Jordanov <d_yugioh@abv.bg>
2019-01-15 18:51:04 +0200

    init

---
+++ 04-Basic Tree Data Structures - Lab/Trees/Tree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Tree<T>
+{
+    public Tree(T value, params Tree<T>[] children)
+    {
+    }
+
+    public void Print(int indent = 0)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Each(Action<T> action)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> OrderDFS()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> OrderBFS()
+    {
+        throw new NotImplementedException();
+    }
+}
