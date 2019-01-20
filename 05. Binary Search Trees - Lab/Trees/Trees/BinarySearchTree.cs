1b10fdc9c653285f5563d2885b9a8946a534e133
Jordan Jordanov <d_yugioh@abv.bg>
2019-01-20 13:42:54 +0200

    init

---
+++ 05. Binary Search Trees - Lab/Trees/Trees/BinarySearchTree.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BinarySearchTree<T> where T : IComparable<T>
+{
+   
+    public void Insert(T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteMin()
+    {
+        throw new NotImplementedException();
+    }
+
+    public BinarySearchTree<T> Search(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> Range(T startRange, T endRange)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Launcher
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
