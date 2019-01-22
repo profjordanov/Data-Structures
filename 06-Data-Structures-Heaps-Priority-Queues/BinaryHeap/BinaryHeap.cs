0e21b7cc170a578dea820bf8a57f4dd2e5c09ba0
Jordan Jordanov <d_yugioh@abv.bg>
2019-01-22 20:16:10 +0200

    init

---
+++ 06-Data-Structures-Heaps-Priority-Queues/BinaryHeap/BinaryHeap.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BinaryHeap<T> where T : IComparable<T>
+{
+    private List<T> heap;
+
+    public BinaryHeap()
+    {
+        // TODO
+    }
+
+    public int Count
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public void Insert(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Peek()
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Pull()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ 06-Data-Structures-Heaps-Priority-Queues/BinaryHeap/Heap.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public static class Heap<T> where T : IComparable<T>
+{
+    public static void Sort(T[] arr)
+    {
+        throw new NotImplementedException();
+    }
+}
