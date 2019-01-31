66fc01ea534a92d12b52a5560cce5ede35eb582d
Jordan Jordanov <d_yugioh@abv.bg>
2019-01-31 12:45:34 +0200

    init

---
+++ 08-Basic Tree and Binary Search Tree - Exercise/BasicTreeDataStructuresExercise/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using static System.Console;
+
+namespace BasicTreeDataStructuresExercise
+{
+    internal class Program
+    {
+        // allows to find the tree nodes during the tree construction
+        private static Dictionary<int, Tree<int>> _nodeByValue = new Dictionary<int, Tree<int>>();
+
+        private static void Main(string[] args)
+        {
+            ReadTree();
+
+            /* 01. Root Node */
+            var root = GetRootNode();
+            WriteLine("-----------------------------");
+            WriteLine($"Root node: {root.Value}");
+            WriteLine("-----------------------------");
+
+            /* 02. Print Tree */
+            PrintTreeIndented(root);
+            WriteLine("-----------------------------");
+
+        }
+
+        private static void ReadTree()
+        {
+            var nodeCount = int.Parse(ReadLine());
+            for (var i = 1; i < nodeCount; i++)
+            {
+                var edge = ReadLine()?.Split();
+                if (edge != null)
+                {
+                    AddEdge(int.Parse(edge[0]), int.Parse(edge[1]));
+                }
+            }
+        }
+
+        //adds an edge to the tree
+        private static void AddEdge(int parent, int child)
+        {
+            var parentNode = GetTreeNodeByValue(parent);
+            var childNode = GetTreeNodeByValue(child);
+
+            parentNode.Children.Add(childNode);
+            childNode.Parent = parentNode;
+        }
+
+        // finds the tree node by its value or create a new node if it does not exist
+        private static Tree<int> GetTreeNodeByValue(int value)
+        {
+            if (!_nodeByValue.ContainsKey(value))
+            {
+                _nodeByValue[value] = new Tree<int>(value);
+            }
+
+            return _nodeByValue[value];
+        }
+
+        private static Tree<int> GetRootNode() =>
+            _nodeByValue.Values.FirstOrDefault(tree => tree.Parent == null);
+
+        // Recursively prints the tree from given node
+        // Format: each level indented + 2 spaces.
+        private static void PrintTreeIndented(Tree<int> node, int indent = 0)
+        {
+            WriteLine(new string(' ', indent * 2) + node.Value);
+
+            foreach (var child in node.Children)
+            {
+                PrintTreeIndented(child, indent + 1);
+            }
+        }
+    }
+}
---
+++ 08-Basic Tree and Binary Search Tree - Exercise/BasicTreeDataStructuresExercise/Tree.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+using static System.Console;
+
+namespace BasicTreeDataStructuresExercise
+{
+    /// <summary>
+    /// Basic Tree Data Structure.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Tree<T>
+    {
+        public T Value { get; set; }
+        public Tree<T> Parent { get; set; }
+        public IList<Tree<T>> Children { get; set; }
+
+        public Tree(T value, params Tree<T>[] children)
+        {
+            Value = value;
+            Children = new List<Tree<T>>(children);
+        }
+
+        /// <summary>
+        /// Prints the tree values on the console.
+        /// </summary>
+        /// <param name="indent"></param>
+        public void Print(int indent = 0)
+        {
+            Write(new string(' ', indent * 2));
+            WriteLine(Value);
+            foreach (var child in Children)
+            {
+                child.Print(indent + 1);
+            }
+        }
+
+        /// <summary>
+        /// Implements an action over the tree structure.
+        /// </summary>
+        /// <param name="action"></param>
+        public void Each(Action<T> action)
+        {
+            action(Value);
+
+            foreach (var child in Children)
+            {
+                child.Each(action);
+            }
+        }
+
+        /// <summary>
+        /// Orders the tree by `Depth-first search`.
+        /// The algorithm starts at the root node
+        /// and explores as far as possible along each branch before backtracking. 
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<T> OrderDfs()
+        {
+            var result = new List<T>();
+            Dfs(this, result);
+            return result;
+        }
+
+        /// <summary>
+        /// Orders the tree by `Breadth-first search`.
+        /// It starts at the tree root and explores all of the neighbor nodes
+        /// at the present depth prior to moving on to the nodes at the next depth level.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<T> OrderBfs()
+        {
+            var result = new List<T>();
+            var queue = new Queue<Tree<T>>();
+
+            queue.Enqueue(this);
+
+            while (queue.TryDequeue(out var current))
+            {
+                result.Add(current.Value);
+                foreach (var child in current.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return result;
+        }
+
+        private static void Dfs(Tree<T> tree, ICollection<T> result)
+        {
+            foreach (var child in tree.Children)
+            {
+                Dfs(child,result);
+            }
+
+            result.Add(tree.Value);
+        }
+
+    }
+}
