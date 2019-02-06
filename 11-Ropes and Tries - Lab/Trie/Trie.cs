75325f792582f4a3821b5f521c46b964d623ebcd
Jordan Jordanov <d_yugioh@abv.bg>
2019-02-06 10:50:42 +0200

    init

--- 11-Ropes and Tries - Lab/Trie/Trie.cs
+++ 11-Ropes and Tries - Lab/Trie/Trie.cs
@@ -1,93 +1,96 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class Trie<Value>
+namespace Trie
 {
-    private Node root;
+	public class Trie<TValue>
+	{
+		private Node _root;
 
-    private class Node
-    {
-        public Value val;
-        public bool isTerminal;
-        public Dictionary<char, Node> next = new Dictionary<char, Node>();
-    }
+		public TValue GetValue(string key)
+		{
+			var x = GetNode(_root, key, 0);
+			if (x == null || !x.IsTerminal)
+			{
+				throw new InvalidOperationException();
+			}
 
-    public Value GetValue(string key)
-    {
-        var x = GetNode(root, key, 0);
-        if (x == null || !x.isTerminal)
-        {
-            throw new InvalidOperationException();
-        }
+			return x.Val;
+		}
 
-        return x.val;
-    }
+		public bool Contains(string key)
+		{
+			var node = GetNode(this._root, key, 0);
+			return node != null && node.IsTerminal;
+		}
 
-    public bool Contains(string key)
-    {
-        var node = GetNode(this.root, key, 0);
-        return node != null && node.isTerminal;
-    }
+		public void Insert(string key, TValue val)
+		{
+			_root = Insert(_root, key, val, 0);
+		}
 
-    public void Insert(string key, Value val)
-    {
-        root = Insert(root, key, val, 0);
-    }
+		public IEnumerable<string> GetByPrefix(string prefix)
+		{
+			var results = new Queue<string>();
+			var x = GetNode(_root, prefix, 0);
 
-    public IEnumerable<string> GetByPrefix(string prefix)
-    {
-        var results = new Queue<string>();
-        var x = GetNode(root, prefix, 0);
-
-        this.Collect(x, prefix, results);
+			this.Collect(x, prefix, results);
         
-        return results;
-    }
+			return results;
+		}
 
-    private Node GetNode(Node x, string key, int d)
-    {
-        if (x == null)
-        {
-            return null;
-        }
+		private Node GetNode(Node x, string key, int d)
+		{
+			if (x == null)
+			{
+				return null;
+			}
 
-        if (d == key.Length)
-        {
-            return x;
-        }
+			if (d == key.Length)
+			{
+				return x;
+			}
 
-        Node node = null;
-        char c = key[d];
+			Node node = null;
+			char c = key[d];
 
-        if (x.next.ContainsKey(c))
-        {
-            node = x.next[c];
-        }
+			if (x.Next.ContainsKey(c))
+			{
+				node = x.Next[c];
+			}
 
-        return GetNode(node, key, d + 1);
-    }
+			return GetNode(node, key, d + 1);
+		}
 
-    private Node Insert(Node x, string key, Value val, int d)
-    {
-       //ToDo: Create insert
-       throw new NotImplementedException();
-    }
+		private Node Insert(Node x, string key, TValue val, int d)
+		{
+			//ToDo: Create insert
+			throw new NotImplementedException();
+		}
 
-    private void Collect(Node x, string prefix, Queue<string> results)
-    {
-        if (x == null)
-        {
-            return;
-        }
+		private void Collect(Node x, string prefix, Queue<string> results)
+		{
+			if (x == null)
+			{
+				return;
+			}
 
-        if (x.val != null && x.isTerminal)
-        {
-            results.Enqueue(prefix);
-        }
+			if (x.Val != null && x.IsTerminal)
+			{
+				results.Enqueue(prefix);
+			}
 
-        foreach (var c in x.next.Keys)
-        {
-            Collect(x.next[c], prefix + c, results);
-        }
-    }
+			foreach (var c in x.Next.Keys)
+			{
+				Collect(x.Next[c], prefix + c, results);
+			}
+		}
+
+		private class Node
+		{
+			public TValue Val;
+			public bool IsTerminal;
+			public Dictionary<char, Node> Next = new Dictionary<char, Node>();
+		}
+	}
 }
--- 11-Ropes and Tries - Lab/Trie/TriePerformanceTester.cs
+++ 11-Ropes and Tries - Lab/Trie/TriePerformanceTester.cs
@@ -4,108 +4,111 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
-public class TriePerformanceTester
+namespace Trie
 {
-    private const string VocabularyPath = "vocabulary.txt";
+	public class TriePerformanceTester
+	{
+		private const string VocabularyPath = "vocabulary.txt";
 
-    public static void Main()
-    {
-        IEnumerable<string> words = LoadWords(VocabularyPath);
-        Console.WriteLine("Words count: {0}", words.Count());
+		public static void Main()
+		{
+			IEnumerable<string> words = LoadWords(VocabularyPath);
+			Console.WriteLine("Words count: {0}", words.Count());
 
-        const int prefixLength = 2;
+			const int prefixLength = 2;
 
-        string[] prefixes = GetAllMatches(Enumerable.Range('A', 'Z' - 'A' + 1).
-            Select(i => (char)i).ToArray(), prefixLength)
-            .ToArray();
+			string[] prefixes = GetAllMatches(Enumerable.Range('A', 'Z' - 'A' + 1).
+					Select(i => (char)i).ToArray(), prefixLength)
+				.ToArray();
 
-        Console.WriteLine("Search prefixes count: {0}", prefixes.Count());
-        Console.WriteLine();
+			Console.WriteLine("Search prefixes count: {0}", prefixes.Count());
+			Console.WriteLine();
 
-        Stopwatch stopWatch = Stopwatch.StartNew();
-        int matchesCount = 0;
-        foreach (string prefix in prefixes)
-        {
-            string[] resultArray = words.Where(w => w.StartsWith(prefix)).ToArray();
-            matchesCount += resultArray.Count();
-        }
-        Console.WriteLine("Found {0} matches", matchesCount);
-        stopWatch.Stop();
+			Stopwatch stopWatch = Stopwatch.StartNew();
+			int matchesCount = 0;
+			foreach (string prefix in prefixes)
+			{
+				string[] resultArray = words.Where(w => w.StartsWith(prefix)).ToArray();
+				matchesCount += resultArray.Count();
+			}
+			Console.WriteLine("Found {0} matches", matchesCount);
+			stopWatch.Stop();
 
-        Console.WriteLine("Regular string matching time: {0} ms", stopWatch.ElapsedMilliseconds);
-        Console.WriteLine();
+			Console.WriteLine("Regular string matching time: {0} ms", stopWatch.ElapsedMilliseconds);
+			Console.WriteLine();
 
-        stopWatch.Restart();
+			stopWatch.Restart();
 
-        Trie<bool> trie = new Trie<bool>();
-        foreach (var item in words)
-        {
-            trie.Insert(item, true);
-        }
+			Trie<bool> trie = new Trie<bool>();
+			foreach (var item in words)
+			{
+				trie.Insert(item, true);
+			}
 
-        stopWatch.Stop();
-        Console.WriteLine("Build trie time: {0} ms", stopWatch.ElapsedMilliseconds);
-        Console.WriteLine();
+			stopWatch.Stop();
+			Console.WriteLine("Build trie time: {0} ms", stopWatch.ElapsedMilliseconds);
+			Console.WriteLine();
 
-        stopWatch.Restart();
-        matchesCount = 0;
+			stopWatch.Restart();
+			matchesCount = 0;
 
-        foreach (string prefix in prefixes)
-        {
-            string[] resultTrie = trie.GetByPrefix(prefix).ToArray();
-            matchesCount += resultTrie.Length;
-        }
+			foreach (string prefix in prefixes)
+			{
+				string[] resultTrie = trie.GetByPrefix(prefix).ToArray();
+				matchesCount += resultTrie.Length;
+			}
 
-        Console.WriteLine("Found {0} matches", matchesCount);
-        stopWatch.Stop();
-        Console.WriteLine("Trie find prefixes time: {0} ms", stopWatch.ElapsedMilliseconds);
-    }
+			Console.WriteLine("Found {0} matches", matchesCount);
+			stopWatch.Stop();
+			Console.WriteLine("Trie find prefixes time: {0} ms", stopWatch.ElapsedMilliseconds);
+		}
 
-    private static IEnumerable<string> GetAllMatches(char[] chars, int length)
-    {
-        int[] indexes = new int[length];
-        char[] current = new char[length];
+		private static IEnumerable<string> GetAllMatches(char[] chars, int length)
+		{
+			int[] indexes = new int[length];
+			char[] current = new char[length];
 
-        for (int i = 0; i < length; i++)
-        {
-            current[i] = chars[0];
-        }
+			for (int i = 0; i < length; i++)
+			{
+				current[i] = chars[0];
+			}
 
-        do
-        {
-            yield return new string(current);
-        }
-        while (Increment(indexes, current, chars));
-    }
+			do
+			{
+				yield return new string(current);
+			}
+			while (Increment(indexes, current, chars));
+		}
 
-    private static bool Increment(int[] indexes, char[] current, char[] chars)
-    {
-        int position = indexes.Length - 1;
+		private static bool Increment(int[] indexes, char[] current, char[] chars)
+		{
+			int position = indexes.Length - 1;
 
-        while (position >= 0)
-        {
-            indexes[position]++;
-            if (indexes[position] < chars.Length)
-            {
-                current[position] = chars[indexes[position]];
-                return true;
-            }
+			while (position >= 0)
+			{
+				indexes[position]++;
+				if (indexes[position] < chars.Length)
+				{
+					current[position] = chars[indexes[position]];
+					return true;
+				}
 
-            indexes[position] = 0;
-            current[position] = chars[0];
-            position--;
-        }
+				indexes[position] = 0;
+				current[position] = chars[0];
+				position--;
+			}
 
-        return false;
-    }
+			return false;
+		}
 
-    /// <summary>
-    /// Returns distinct set of words. <remarks>This method returns 58110 English words.</remarks>
-    /// </summary>
-    /// <returns>Distinct set of words.</returns>
-    private static IEnumerable<string> LoadWords(string fileName)
-    {
-        string path = Path.Combine(@"..\..\", fileName);
-        return File.ReadAllLines(path);
-    }
+		/// <summary>
+		/// Returns distinct set of words. <remarks>This method returns 58110 English words.</remarks>
+		/// </summary>
+		/// <returns>Distinct set of words.</returns>
+		private static IEnumerable<string> LoadWords(string fileName)
+		{
+			string path = Path.Combine(@"..\..\", fileName);
+			return File.ReadAllLines(path);
+		}
+	}
 }
