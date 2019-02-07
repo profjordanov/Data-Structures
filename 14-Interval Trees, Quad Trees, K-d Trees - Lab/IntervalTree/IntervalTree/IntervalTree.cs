47f11822c4e7ecad6adcc151a11e94943edd5331
Jordan Jordanov <d_yugioh@abv.bg>
2019-02-07 10:19:40 +0200

    init

---
+++ 14-Interval Trees, Quad Trees, K-d Trees - Lab/IntervalTree/IntervalTree/IntervalTree.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class IntervalTree
+{
+    private class Node
+    {
+        internal Interval interval;
+        internal double max;
+        internal Node right;
+        internal Node left;
+
+        public Node(Interval interval)
+        {
+            this.interval = interval;
+            this.max = interval.Hi;
+        }
+    }
+
+    private Node root;
+
+    public void Insert(double lo, double hi)
+    {
+        this.root = this.Insert(this.root, lo, hi);
+    }
+
+    public void EachInOrder(Action<Interval> action)
+    {
+        EachInOrder(this.root, action);
+    }
+
+    public Interval SearchAny(double lo, double hi)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Interval> SearchAll(double lo, double hi)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void EachInOrder(Node node, Action<Interval> action)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        EachInOrder(node.left, action);
+        action(node.interval);
+        EachInOrder(node.right, action);
+    }
+
+    private Node Insert(Node node, double lo, double hi)
+    {
+        if (node == null)
+        {
+            return new Node(new Interval(lo, hi));
+        }
+
+        int cmp = lo.CompareTo(node.interval.Lo);
+        if (cmp < 0)
+        {
+            node.left = Insert(node.left, lo, hi);
+        }
+        else if (cmp > 0)
+        {
+            node.right = Insert(node.right, lo, hi);
+        }
+        
+        return node;
+    }
+}
