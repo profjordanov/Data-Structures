40997d962324277175040ad36a420d5fc9f17b34
Jordan Jordanov <d_yugioh@abv.bg>
2019-02-18 13:51:05 +0200

    init

---
+++ 14-Interval Trees, Quad Trees, K-d Trees - Lab/KdTree/KdTree/Node.cs
@@ -0,0 +1,11 @@
+﻿public class Node
+{
+	public Node(Point2D point)
+	{
+		Point = point;
+	}
+
+	public Point2D Point { get; set; }
+	public Node Left { get; set; }
+	public Node Right { get; set; }
+}
