83bc557efcdb4a5d52d131319f59a4adf3d6dbb5
Jordan Jordanov <d_yugioh@abv.bg>
2019-02-22 11:00:08 +0200

    init

---
+++ 17-PersonCollection/PersonCollection/Person.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Person
+{
+    public string Email { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Town { get; set; }
+}
