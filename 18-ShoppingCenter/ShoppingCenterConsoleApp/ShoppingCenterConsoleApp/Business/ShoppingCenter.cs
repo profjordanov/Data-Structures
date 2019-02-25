2fa9cf8aaba17f44b3a192642bd8ac7de423cb68
Jordan Jordanov <d_yugioh@abv.bg>
2019-02-25 10:38:25 +0200

    init

---
+++ 18-ShoppingCenter/ShoppingCenterConsoleApp/ShoppingCenterConsoleApp/Business/ShoppingCenter.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ShoppingCenterConsoleApp.Core;
+using Wintellect.PowerCollections;
+
+namespace ShoppingCenterConsoleApp.Business
+{
+	/// <summary>
+	/// Data structure holding the <see cref="Product"/>.
+	/// </summary>
+	public class ShoppingCenter : IShoppingCenter
+	{
+		private readonly Dictionary<string, OrderedBag<Product>> _byProducer 
+			= new Dictionary<string, OrderedBag<Product>>();
+
+		private readonly Dictionary<string, OrderedBag<Product>> _byName 
+			= new Dictionary<string, OrderedBag<Product>>();
+
+		private readonly Dictionary<string, OrderedBag<Product>> _byNameAndProducer
+			= new Dictionary<string, OrderedBag<Product>>();
+
+		private readonly SortedDictionary<decimal, OrderedBag<Product>> _byPrice 
+			= new SortedDictionary<decimal, OrderedBag<Product>>();
+
+		/// <summary>
+		/// Adds a product by given name, price and producer.
+		/// If a product with the same name / producer/ price already exists,
+		/// the newly added product does not affect the existing ones (duplicates are allowed). 
+		/// </summary>
+		/// <param name="product"></param>
+		public void AddProduct(Product product)
+		{
+			if (product == null)
+			{
+				return;
+			}
+
+			AddToDictionary(_byProducer, product.Producer, product);
+			AddToDictionary(_byName, product.Name, product);
+			AddToDictionary(_byNameAndProducer, GetNameProducer(product.Name,product.Producer), product);
+			AddToDictionary(_byPrice, product.Price, product);
+		}
+
+		/// <summary>
+		/// Deletes all products matching given producer. 
+		/// </summary>
+		/// <param name="producer"></param>
+		/// <returns>Count of deleted items.</returns>
+		public int DeleteProductsByProducer(string producer)
+		{
+			if (!_byProducer.ContainsKey(producer))
+			{
+				return 0;
+			}
+
+			var count = 0;
+			_byProducer[producer]
+				.ToList()
+				.ForEach(product =>
+				{
+					_byProducer[producer].Remove(product);
+					_byName[product.Name].Remove(product);
+					_byNameAndProducer[GetNameProducer(product.Name, producer)].Remove(product);
+					_byPrice[product.Price].Remove(product);
+					count++;
+				});
+
+			return count;
+		}
+
+		/// <summary>
+		/// Deletes all products matching given product name and producer. 
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="producer"></param>
+		/// <returns></returns>
+		public int DeleteProductsByNameAndProducer(string name, string producer)
+		{
+			var nameProducer = GetNameProducer(name, producer);
+			if(!_byNameAndProducer.ContainsKey(nameProducer))
+			{
+				return 0;
+			}
+
+			var count = 0;
+			_byNameAndProducer[ nameProducer ]
+				.ToList()
+				.ForEach(p =>
+				{
+					_byName[ name ].Remove(p);
+					_byProducer[ producer ].Remove(p);
+					_byNameAndProducer[ nameProducer ].Remove(p);
+					_byPrice[ p.Price ].Remove(p);
+					count++;
+				});
+
+			return count;
+		}
+
+		/// <param name="name"></param>
+		/// <returns>All products by given product name.</returns>
+		public IEnumerable<Product> FindProductsByName(string name) =>
+			!string.IsNullOrEmpty(name) && _byName.ContainsKey(name)
+				? _byName[name].ToList()
+				: new List<Product>();
+
+		/// <param name="producer"></param>
+		/// <returns>All products by given producer.</returns>
+		public IEnumerable<Product> FindProductsByProducer(string producer) =>
+			!string.IsNullOrEmpty(producer) && _byProducer.ContainsKey(producer)
+				? _byProducer[producer].ToList()
+				: new List<Product>();
+
+		/// <param name="startPrice"></param>
+		/// <param name="endPrice"></param>
+		/// <returns>All products whose price is greater or equal than fromPrice and less or equal than toPrice</returns>
+		public IEnumerable<Product> FindProductsByPriceRange(decimal startPrice, decimal endPrice)
+		{
+			var result = new OrderedBag<Product>();
+
+			_byPrice
+				.Keys
+				.Where(price => startPrice <= price && price <= endPrice)
+				.ToList()
+				.ForEach(price => result.AddMany(_byPrice[ price ]));
+
+			return result;
+		}
+
+		// Helper methods
+		private static void AddToDictionary<T>(IDictionary<T, OrderedBag<Product>> dictionary, T key, Product product)
+		{
+			if (!dictionary.ContainsKey(key))
+			{
+				dictionary[key] = new OrderedBag<Product>();
+			}
+
+			dictionary[key].Add(product);
+		}
+
+		private static string GetNameProducer(string name, string producer) => $"{name}&&{producer}";
+	}
+}
---
+++ 18-ShoppingCenter/ShoppingCenterConsoleApp/ShoppingCenterConsoleApp/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ShoppingCenterConsoleApp.Business;
+using ShoppingCenterConsoleApp.Core;
+
+namespace ShoppingCenterConsoleApp
+{
+	internal class Program
+    {
+	    private static void Main()
+        {
+	        var shoppingCenter = new ShoppingCenter();
+	        var linesCount = int.Parse(Console.ReadLine());
+	        for (var i = 0; i < linesCount; i++)
+	        {
+				var line = Console.ReadLine(); // {command} {arg1};{arg2};{...}
+				var tokens = line?.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+				if(tokens?.Length < 2) // {command} {args}
+				{
+					continue;
+				}
+
+				var command = tokens[ 0 ];
+				var args = GetCommandArgs(line);
+
+				switch(command)
+				{
+					case "AddProduct":
+						AddProduct(shoppingCenter, args);
+						break;
+					case "DeleteProducts":
+						DeleteProducts(shoppingCenter, args);
+						break;
+					case "FindProductsByName":
+						FindProductsByName(shoppingCenter, args);
+						break;
+					case "FindProductsByProducer":
+						FindProductsByProducer(shoppingCenter, args);
+						break;
+					case "FindProductsByPriceRange":
+						FindProductsByPriceRange(shoppingCenter, args);
+						break;
+				}
+			}
+		}
+
+		// Commands
+	    private static void FindProductsByPriceRange(IShoppingCenter shoppingCenter, IReadOnlyList<string> args)
+	    {
+		    if (args.Count < 2)
+		    {
+			    return;
+		    }
+
+			var startPrice = decimal.Parse(args[ 0 ]);
+			var endPrice = decimal.Parse(args[ 1 ]);
+			var products = shoppingCenter.FindProductsByPriceRange(startPrice, endPrice);
+			PrintSearchResult(products.ToArray());
+		}
+
+	    private static void FindProductsByProducer(IShoppingCenter shoppingCenter, IReadOnlyList<string> args)
+	    {
+		    if (!args.Any())
+		    {
+			    return;
+		    }
+
+			var result = shoppingCenter.FindProductsByProducer(args[ 0 ]);
+			PrintSearchResult(result.ToArray());
+		}
+
+	    private static void FindProductsByName(IShoppingCenter shoppingCenter, IReadOnlyList<string> args)
+	    {
+		    if (!args.Any())
+		    {
+			    return;
+		    }
+
+		    var result = shoppingCenter.FindProductsByName(args[ 0 ]);
+		    PrintSearchResult(result.ToArray());
+		}
+
+	    private static void DeleteProducts(IShoppingCenter shoppingCenter, IReadOnlyList<string> args)
+	    {
+		    if (!args.Any())
+		    {
+			    return;
+		    }
+
+		    var count = 0;
+		    if(args.Count > 1)
+		    {
+			    var name = args[ 0 ];
+			    var producer = args[ 1 ];
+			    count = shoppingCenter.DeleteProductsByNameAndProducer(name, producer);
+		    }
+		    else
+		    {
+			    count = shoppingCenter.DeleteProductsByProducer(args[ 0 ]);
+		    }
+
+		    PrintDeletionResult(count);
+		}
+
+	    private static void AddProduct(IShoppingCenter shoppingCenter, IReadOnlyList<string> args)
+	    {
+		    if(args.Count < 3)
+		    {
+			    return;
+		    }
+
+		    var name = args[ 0 ];
+		    var price = decimal.Parse(args[ 1 ]);
+		    var producer = args[ 2 ];
+		    var product = new Product(name, price, producer);
+		    shoppingCenter.AddProduct(product);
+		    Console.WriteLine("Product added");
+	    }
+
+		// Helper methods
+		private static string[] GetCommandArgs(string line)
+	    {
+			var args = line.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+			// Strip command from arg1: {command} {arg1} => {arg1}
+			var firstArg = args[ 0 ];
+			args[ 0 ] = firstArg.Substring(firstArg.IndexOf(' ') + 1).TrimStart();
+			return args;
+		}
+
+		private static void PrintDeletionResult(int count) =>
+			Console.WriteLine(count != 0
+				? $"{count} products deleted"
+				: "No products found");
+
+		private static void PrintSearchResult(IReadOnlyCollection<Product> products)
+			=> Console.WriteLine(products.Any()
+				? string.Join(Environment.NewLine, products)
+				: "No products found");
+	}
+}
