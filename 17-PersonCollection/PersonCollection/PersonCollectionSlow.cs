e1b794a46eda6d1cf8cf4db99c700da5f7b38767
Jordan Jordanov <d_yugioh@abv.bg>
2019-02-25 09:51:55 +0200

    init

--- 17-PersonCollection/PersonCollection/PersonCollectionSlow.cs
+++ 17-PersonCollection/PersonCollection/PersonCollectionSlow.cs
@@ -1,59 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class PersonCollectionSlow : IPersonCollection
 {
-    // TODO: define the underlying data structures here ...
+    // underlying data structures
 
-    public bool AddPerson(string email, string name, int age, string town)
+	private List<Person> people = new List<Person>();
+
+	// creates a new person and appends it to the underlying list
+	public bool AddPerson(string email, string name, int age, string town)
     {
-        // TODO: implement this
-        throw new NotImplementedException();
+		var person = new Person(email, name, age, town);
+		people.Add(person);
+		return true;
     }
 
-    public int Count
-    {
-        get
-        {
-            // TODO: implement this
-            throw new NotImplementedException();
-        }
-    }
+	public int Count => people.Count;
 
-    public Person FindPerson(string email)
-    {
-        // TODO: implement this
-        throw new NotImplementedException();
-    }
+	public Person FindPerson(string email) =>
+		people.FirstOrDefault(person => person.Email == email);
 
-    public bool DeletePerson(string email)
-    {
-        // TODO: implement this
-        throw new NotImplementedException();
-    }
+	public bool DeletePerson(string email) =>
+		people.Remove(FindPerson(email));
 
-    public IEnumerable<Person> FindPersons(string emailDomain)
-    {
-        // TODO: implement this
-        throw new NotImplementedException();
-    }
+	// finds persons by email domain
+	public IEnumerable<Person> FindPersons(string emailDomain) =>
+		people
+			.Where(person => person.Email.EndsWith("@" + emailDomain))
+			.OrderBy(person => person.Email);
 
-    public IEnumerable<Person> FindPersons(string name, string town)
-    {
-        // TODO: implement this
-        throw new NotImplementedException();
-    }
+	// finds persons by name and town
+	public IEnumerable<Person> FindPersons(string name, string town) =>
+		people
+			.Where(person => person.Name == name &&
+			                 person.Town == town)
+			.OrderBy(person => person.Email);
 
-    public IEnumerable<Person> FindPersons(int startAge, int endAge)
-    {
-        // TODO: implement this
-        throw new NotImplementedException();
-    }
+	// finds by age range
+	public IEnumerable<Person> FindPersons(int startAge, int endAge) =>
+		people.Where(person => person.Age >= startAge &&
+		                       person.Age <= endAge)
+			.OrderBy(person => person.Age)
+			.ThenBy(person => person.Email);
 
     public IEnumerable<Person> FindPersons(
-        int startAge, int endAge, string town)
-    {
-        // TODO: implement this
-        throw new NotImplementedException();
-    }
+        int startAge, 
+        int endAge, 
+        string town) =>
+	    people.Where(person => (person.Age >= startAge &&
+	                            person.Age <= endAge) &&
+	                           person.Town == town)
+		    .OrderBy(person => person.Age)
+		    .ThenBy(person => person.Email);
 }
